6bfbde6236a786b73d45cddc9dff274b28fa1a0e
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-07-16 15:41:30 +0200

    clean test

--- src/test/kotlin/com/battleships/BattleshipsGameAppTest.kt
+++ src/test/kotlin/com/battleships/BattleshipsGameAppTest.kt
@@ -12,49 +12,8 @@ class BattleshipsGameAppTest {
     private val parser: Parser = mockk(relaxed = true)
     private val game = BattleshipsGameApp(ioStream, parser, battleshipGame)
 
-
     @Test
-    fun `when application starts read from input`() {
-        game.start()
-
-        verify { ioStream.readInput() }
-    }
-
-    @Test
-    fun `parse input to retrieve current command`() {
-        every { ioStream.readInput() } returns "add Paperino"
-
-        game.start()
-
-        verify { parser.parse("add Paperino") }
-    }
-
-    @Test
-    fun `execute command`() {
-        val mockCommand = mockk<Command>(relaxed = true)
-
-        every { ioStream.readInput() } returns "add Paperino"
-        every { parser.parse("add Paperino") } returns mockCommand
-
-        game.start()
-
-        verify { mockCommand.execute() }
-    }
-
-    @Test
-    fun `ask for status after command execution`() {
-        val mockCommand = mockk<Command>(relaxed = true)
-
-        every { ioStream.readInput() } returns "add Paperino"
-        every { parser.parse("add Paperino") } returns mockCommand
-
-        game.start()
-
-        verify { battleshipGame.status }
-    }
-
-    @Test
-    fun `write status`() {
+    fun `execute a command and write the current status`() {
         val mockCommand = mockk<Command>(relaxed = true)
 
         every { ioStream.readInput() } returns "add Paperino"
